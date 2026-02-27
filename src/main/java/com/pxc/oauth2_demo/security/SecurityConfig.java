09eedb1961368cf6dae185a02aa6701c9ad346ba
Iqbal Hossain <iqbal.h.rasel@gmail.com>
2026-02-27 16:22:35 +0600

    added security filter chain with oauth and cors

--- src/main/java/com/pxc/oauth2_demo/security/SecurityConfig.java
+++ src/main/java/com/pxc/oauth2_demo/security/SecurityConfig.java
@@ -1,9 +1,47 @@
 package com.pxc.oauth2_demo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+
+        http.cors(c -> c.configurationSource(corsConfigurationSource()));
+        http.csrf(c -> c.disable());
+        http.authorizeHttpRequests(a -> a
+                .requestMatchers("/hello").permitAll()
+                .anyRequest()
+                .authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
